16211613755d31ecd4177498ec08ec8b76770ffc
ynwd <ynwd@users.noreply.github.com>
2020-05-22 01:03:27 +0700

    deno fmt

--- benchmarks/deno_http.ts
+++ benchmarks/deno_http.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.52.0/http/server.ts";
-const port = 3005
+const port = 3005;
 const s = serve({ port });
 console.log("deno_http listen on", port);
 for await (const req of s) {
