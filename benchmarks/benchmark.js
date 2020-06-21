d9a5d0e0b35a6ac12a9547568c84b5256c80b7e0
ynwd <ynwd@users.noreply.github.com>
2020-06-21 18:14:25 +0700

    update benchmark

--- benchmarks/benchmark.js
+++ benchmarks/benchmark.js
@@ -3,11 +3,11 @@ const { execSync } = require("child_process");
 
 const oak_version = "5.2.0";
 const express_version = "4.17.1";
-const fastro_version = "0.11.4";
+const fastro_version = "0.12.0";
 const abc_version = "1.0.0-rc10";
 const deno_version = "1.1.1";
 const node_version = "14.3.0";
-const fastify_version = "2.14.1";
+const fastify_version = "2.15.0";
 const php_version = "7.3.11";
 const flask_version = "1.1.2";
 
