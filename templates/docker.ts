6a5c4937e4c1cdf1e55016af1df202d4c0964302
ynwd <hello@fastro.dev>
2020-10-24 17:28:52 +0700

    fix

--- templates/docker.ts
+++ templates/docker.ts
@@ -2,15 +2,18 @@ import { DOCKER_VERSION, FASTRO_VERSION } from "../core/types.ts";
 
 export const docker = `FROM hayd/alpine-deno:${DOCKER_VERSION}
 
+WORKDIR /app
+
+USER deno
+
+COPY . ./
+
 # This command will download all the necessary files and cache them 
 # so they are not downloaded again when the application is restarted.
 RUN deno cache https://raw.fastro.dev/v${FASTRO_VERSION}/mod.ts
+RUN deno cache https://raw.fastro.dev/v${FASTRO_VERSION}/cli/mod.ts
 RUN deno cache https://raw.fastro.dev/v${FASTRO_VERSION}/deps.ts
 RUN deno cache https://raw.fastro.dev/v${FASTRO_VERSION}/cli/fastro.ts
-
-WORKDIR /app
-
-COPY . ./
 
 CMD ["run", "--no-check", "-A", "https://raw.fastro.dev/v${FASTRO_VERSION}/cli/fastro.ts", "serve", "--port", "8080", "--production"]
 `;
