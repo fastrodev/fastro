6e4813dbec26a712d4503064acd8b4d7bee2d412
yanu <10122431+ynwd@users.noreply.github.com>
2021-02-20 16:06:53 +0700

    fix cli

--- templates/settings.ts
+++ templates/settings.ts
@@ -1,25 +1,31 @@
 export const setting = `{
-  "files.eol": "\\n",
+  "deno.enable": true,
+  "deno.codeLens.implementations":true,
+  "deno.codeLens.references":true,
+  "deno.codeLens.referencesAllFunctions":true,
+  "deno.config": null,
+  "deno.importMap": null,
+  "deno.lint": true,
+  "deno.unstable": true,
   "files.trimTrailingWhitespace": true,
   "[javascript]": {
-    "editor.defaultFormatter": "vscode.typescript-language-features"
+    "editor.defaultFormatter": "denoland.vscode-deno"
   },
   "[typescript]": {
-    "editor.defaultFormatter": "vscode.typescript-language-features",
+    "editor.defaultFormatter": "denoland.vscode-deno",
     "editor.formatOnSave": true,
     "editor.codeActionsOnSave": {
       "source.organizeImports": true,
     }
   },
   "[typescriptreact]": {
-    "editor.defaultFormatter": "vscode.typescript-language-features",
+    "editor.defaultFormatter": "denoland.vscode-deno",
     "editor.formatOnSave": true,
     "editor.codeActionsOnSave": {
       "source.organizeImports": true,
     }
   },
-  "deno.enable": true,
-  "deno.unstable": true,
-  "editor.tabSize": 2
+  "editor.tabSize": 2,
+  "files.eol": "\\n"
 }
 `;
