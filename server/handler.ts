6c13103ff3ec4aa27e4ac02365b58a2e75e110b0
ynwd <10122431+ynwd@users.noreply.github.com>
2022-03-17 22:46:02 +0700

    add constant

--- server/handler.ts
+++ server/handler.ts
@@ -4,9 +4,15 @@ import { Handler, ConnInfo } from "./deps.ts"
 const NOT_FOUND_STRING = 'URL not found'
 const NOT_FOUND_CODE = 404
 const UNDEFINED_MIDDLEWARE = 'Undefined middleware'
+const EMPTY = ''
+const COLON = ':'
+const SLASH = '/'
+const DOUBLE_SLASH = '//'
+const HASHTAG = '#'
 const routerMap: Map<string, FinalRoute> = new Map()
 
-let hostname = ''
+let hostname = EMPTY
+
 export interface FinalRoute {
   method: string
   path: string | RegExp
@@ -19,11 +25,11 @@ export interface FinalRoute {
 function initReouterMap(
   map: Map<string, Route>,
   url: string) {
-  const [http, path] = url.split('//')
-  const [host] = path.split('/')
-  hostname = `${http}//${host}`
+  const [http, path] = url.split(DOUBLE_SLASH)
+  const [host] = path.split(SLASH)
+  hostname = `${http}${DOUBLE_SLASH}${host}`
   map.forEach((v, k) => {
-    const [method, , kpath] = k.split('#')
+    const [method, , kpath] = k.split(HASHTAG)
     const key = `${method}:${hostname}${kpath}`
     const route = {
       path: v.path,
@@ -87,46 +93,42 @@ const handleMiddleware = (req: Request, connInfo: ConnInfo, middleware?: Middlew
 }
 
 function createMapKey(req: Request): string {
-  let result = ''
+  let result = EMPTY
   routerMap.forEach((v) => {
     if (v.method === req.method && validateURL(v.url, req.url, v.host)) {
-      result = `${req.method}:${v.url}`
+      result = `${req.method}${COLON}${v.url}`
     }
   })
   return result
 }
 
 function validateURL(routeURL: string, incomingURL: string, host: string) {
-  const routePath = routeURL.replace(host, '')
-  const incomingPath = incomingURL.replace(host, '')
-  const r = routePath.split('/')
-  const i = incomingPath.split('/')
-  if (r.length != i.length) return false
-  const res = parsePath(r, i)
-  console.log
-  return res
+  const routePath = routeURL.replace(host, EMPTY).split(SLASH)
+  const incomingPath = incomingURL.replace(host, EMPTY).split(SLASH)
+  if (routePath.length != incomingPath.length) return false
+  return parsePath(routePath, incomingPath)
 }
 
 function parsePath(route: string[], incoming: string[]) {
   route.shift(); incoming.shift()
   for (let idx = 0; idx < route.length; idx++) {
     const path = route[idx]
-    if (checkPath(path, incoming, idx)) return false
+    if (!isValidPath(path, incoming, idx)) return false
   }
   return true
 }
 
-function checkPath(path: string, incoming: string[], idx: number) {
-  return !(path === incoming[idx] || regex(incoming[idx], path))
+function isValidPath(path: string, incoming: string[], idx: number) {
+  return (path === incoming[idx] || regex(incoming[idx], path))
 }
 
 function regex(_incoming: string, path: string) {
-  if (path.charAt(0) === ':') return true
+  if (path.charAt(0) === COLON) return true
   return false
 }
 
 function getRoute(req: Request): string {
-  let result = ''
+  let result = EMPTY
   routerMap.forEach((v) => {
     if (v.method === req.method && validateURL(v.url, req.url, v.host)) {
       result = `${v.url}`
@@ -136,15 +138,15 @@ function getRoute(req: Request): string {
 }
 
 function extractParams(req: Request) {
-  const routeParams = getRoute(req).replace(hostname, '').split('/')
-  const params = req.url.replace(hostname, '').split('/')
+  const routeParams = getRoute(req).replace(hostname, EMPTY).split(SLASH)
+  const params = req.url.replace(hostname, EMPTY).split(SLASH)
   routeParams.shift(); params.shift()
   return routeParams
     .map((param, index) => { return { param, index } })
-    .filter((val) => val.param.charAt(0) === ':')
+    .filter((val) => val.param.charAt(0) === COLON)
     .map((val) => {
       return {
-        name: val.param.replace(':', ''),
+        name: val.param.replace(COLON, EMPTY),
         value: params[val.index]
       }
     })
