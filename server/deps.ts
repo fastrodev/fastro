1789302f58f7d6752fb7e87b6ab63fd73c8d5c11
yanu <10122431+ynwdd@users.noreply.github.com>
2022-04-08 22:14:30 +0700

    update deps

--- server/deps.ts
+++ server/deps.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/std@0.133.0/http/server.ts";
-export * from "https://deno.land/std@0.133.0/http/http_status.ts";
-export * from "https://deno.land/std@0.133.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.134.0/http/server.ts"
+export * from "https://deno.land/std@0.134.0/http/http_status.ts"
+export * from "https://deno.land/std@0.134.0/testing/asserts.ts"
