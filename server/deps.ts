ead03594d4a4366fb0a52d0b963b78e8f3974084
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-02-11 17:00:10 +0700

    remove react types

--- server/deps.ts
+++ server/deps.ts
@@ -4,6 +4,5 @@ export { serve, Server } from "$std/http/server.ts";
 export type { ConnInfo, ServeInit } from "$std/http/server.ts";
 export { h, ReactDOMServer };
 
-// @deno-types="https://cdn.esm.sh/v78/@types/react@18.0.9/react.d.ts"
 import ReactDOMServer from "https://esm.sh/react-dom@18.2.0/server";
 import { createElement as h } from "https://esm.sh/react@18.2.0";
