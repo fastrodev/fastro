160dbf5a2c76ac91baecdbddbffdbd50849ebcbb
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-02-25 17:03:32 +0700

    std@0.178.0

--- server/deps.ts
+++ server/deps.ts
@@ -1,13 +1,13 @@
-export * from "https://deno.land/std@0.177.0/http/cookie.ts";
+export * from "https://deno.land/std@0.178.0/http/cookie.ts";
 export {
   Status,
   STATUS_TEXT,
-} from "https://deno.land/std@0.177.0/http/http_status.ts";
-export { serve, Server } from "https://deno.land/std@0.177.0/http/server.ts";
+} from "https://deno.land/std@0.178.0/http/http_status.ts";
+export { serve, Server } from "https://deno.land/std@0.178.0/http/server.ts";
 export type {
   ConnInfo,
   ServeInit,
-} from "https://deno.land/std@0.177.0/http/server.ts";
+} from "https://deno.land/std@0.178.0/http/server.ts";
 export { h, React, ReactDOMServer };
 
 import ReactDOMServer from "https://esm.sh/react-dom@18.2.0/server";
