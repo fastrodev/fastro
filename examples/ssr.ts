3de99117ed77fd05735e297a09719b2204b66810
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-03-14 16:22:57 +0700

    update ssr-examples

--- examples/ssr.ts
+++ examples/ssr.ts
@@ -8,37 +8,37 @@ const hello = createSSR(Hello);
 const app = createSSR(App);
 const user = createSSR(User);
 
-const f = fastro();
-
-// SSR page initiation
-f.static("/public")
+const f = fastro()
   .page("/", hello, (_req: HttpRequest, res: HttpResponse) => {
-    return initSSR(res, hello)
+    const desc = "Fastro React SSR Example";
+    return initSSR(hello, res)
+      .ogImage("https://deno.land/images/artwork/v1.png")
+      .metaDesc(desc)
       .title(`Hello`)
+      .lang("EN")
       .render();
   })
   .page("/:user", user, (req: HttpRequest, res: HttpResponse) => {
     const data = req.match?.pathname.groups.user;
-    return initSSR(res, user)
+    return initSSR(user, res)
       .props({ data })
       .render();
   })
-  .page("/:user/count", app, (req: HttpRequest, res: HttpResponse) => {
+  .page("/:user/app", app, (req: HttpRequest, res: HttpResponse) => {
     const data = req.match?.pathname.groups.user;
-    return initSSR(res, app)
+    return initSSR(app, res)
       .title(`Hello ${data}`)
       .props({ data })
       .render();
   });
 
-// API initiation
 f.get("/api", (_req: HttpRequest, res: HttpResponse) => {
   return res.json({ msg: "hello" });
 });
 
 await f.serve();
 
-function initSSR(res: HttpResponse, ssr: SSR) {
+function initSSR(ssr: SSR, res: HttpResponse) {
   const [css, js, meta, center, dark] = [
     `href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"`,
     `src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"`,
--- pages/user.tsx
+++ pages/user.tsx
@@ -3,7 +3,7 @@ import React from "https://esm.sh/react@18.2.0";
 // you can access injected props from server directly
 const User = (props: { data: string }) => (
   <h1>
-    <a href={props.data + "/count"}>Hello {props.data}</a>
+    <a href={props.data + "/app"}>Hello {props.data}</a>
   </h1>
 );
 
