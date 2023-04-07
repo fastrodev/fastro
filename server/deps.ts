07ff6a53f70d58a804ceb665da1acb0dae8cd375
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-04-08 05:00:15 +0700

    std@0.182.0

--- server/deps.ts
+++ server/deps.ts
@@ -1,16 +1,16 @@
-export * from "https://deno.land/std@0.179.0/http/cookie.ts";
+export * from "https://deno.land/std@0.182.0/http/cookie.ts";
 export {
   Status,
   STATUS_TEXT,
-} from "https://deno.land/std@0.179.0/http/http_status.ts";
-export { serve, Server } from "https://deno.land/std@0.179.0/http/server.ts";
+} from "https://deno.land/std@0.182.0/http/http_status.ts";
+export { serve, Server } from "https://deno.land/std@0.182.0/http/server.ts";
 export type {
   ConnInfo,
   ServeInit,
-} from "https://deno.land/std@0.179.0/http/server.ts";
-export * from "https://deno.land/std@0.179.0/media_types/mod.ts";
-export * from "https://deno.land/std@0.179.0/path/mod.ts";
-export * from "https://deno.land/std@0.179.0/testing/asserts.ts";
+} from "https://deno.land/std@0.182.0/http/server.ts";
+export * from "https://deno.land/std@0.182.0/media_types/mod.ts";
+export * from "https://deno.land/std@0.182.0/path/mod.ts";
+export * from "https://deno.land/std@0.182.0/testing/asserts.ts";
 export { h, React, ReactDOMServer };
 
 import ReactDOMServer from "https://esm.sh/react-dom@18.2.0/server";
