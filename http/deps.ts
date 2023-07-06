65e0cffb361b7ff91077d8edd068506ef6702805
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-07 02:19:12 +0700

    deps.ts: asserts

--- http/deps.ts
+++ http/deps.ts
@@ -24,4 +24,4 @@ export type {
   Handler,
 } from "https://deno.land/std@0.193.0/http/server.ts";
 
-export * from "https://deno.land/std/testing/asserts.ts";
+export * from "https://deno.land/std@0.193.0/testing/asserts.ts";
