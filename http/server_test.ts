a9fec5e24979535c79a90e22c3ba442d8bfd27b9
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-07 14:12:25 +0700

    test: add allow env

--- http/server_test.ts
+++ http/server_test.ts
@@ -3,34 +3,37 @@ import { assertEquals } from "./deps.ts";
 
 const host = "http://localhost:8000";
 
-Deno.test({ permissions: { net: true } }, async function getResponse() {
-  const f = fastro();
-  f.get("/", () => new Response("get"));
-  f.post("/", () => new Response("post"));
-  f.put("/", () => new Response("put"));
-  f.delete("/", () => new Response("delete"));
-  f.options("/", () => new Response("options"));
-  f.head("/", () => new Response(""));
-  f.serve();
+Deno.test(
+  { permissions: { net: true, env: true } },
+  async function getResponse() {
+    const f = fastro();
+    f.get("/", () => new Response("get"));
+    f.post("/", () => new Response("post"));
+    f.put("/", () => new Response("put"));
+    f.delete("/", () => new Response("delete"));
+    f.options("/", () => new Response("options"));
+    f.head("/", () => new Response(""));
+    f.serve();
 
-  const get = await fetch(host, { method: "GET" });
-  assertEquals(await get.text(), "get");
+    const get = await fetch(host, { method: "GET" });
+    assertEquals(await get.text(), "get");
 
-  const post = await fetch(host, { method: "POST" });
-  assertEquals(await post.text(), "post");
+    const post = await fetch(host, { method: "POST" });
+    assertEquals(await post.text(), "post");
 
-  const put = await fetch(host, { method: "PUT" });
-  assertEquals(await put.text(), "put");
+    const put = await fetch(host, { method: "PUT" });
+    assertEquals(await put.text(), "put");
 
-  const del = await fetch(host, { method: "DELETE" });
-  assertEquals(await del.text(), "delete");
+    const del = await fetch(host, { method: "DELETE" });
+    assertEquals(await del.text(), "delete");
 
-  const ops = await fetch(host, { method: "OPTIONS" });
-  assertEquals(await ops.text(), "options");
+    const ops = await fetch(host, { method: "OPTIONS" });
+    assertEquals(await ops.text(), "options");
 
-  const head = await fetch(host, { method: "HEAD" });
-  assertEquals(await head.text(), "");
+    const head = await fetch(host, { method: "HEAD" });
+    assertEquals(await head.text(), "");
 
-  f.close();
-  await f.finished();
-});
+    f.close();
+    await f.finished();
+  },
+);
