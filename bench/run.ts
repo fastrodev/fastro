35b01eb9fb14b3dc913c640754199f7ba35fb7a7
ynwd <10122431+ynwd@users.noreply.github.com>
2023-07-09 11:06:51 +0700

    Update run.ts

--- bench/run.ts
+++ bench/run.ts
@@ -118,8 +118,8 @@ const formattedDate = date.toLocaleDateString("en-US", {
 });
 
 let markdown = `---
-title: "Fastro internal benchmarks"
-description: This is the final result of an internal benchmarks running on a github action
+title: "Internal benchmarks"
+description: This is the final result of internal benchmarks running on a github action
 image: https://fastro.dev/static/image.png
 author: Yanu Widodo
 date: ${formattedDate}
