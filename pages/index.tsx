43ea8886de4b3cdc7ac9f517e6e6bd0403a40991
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-12 09:59:50 +0700

    responsive item

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,13 @@ const Index = (props: { version: string }) => {
       </header>
 
       <main className="px-3">
-        <h1 className="display-4 fw-bold">
+        <h1 className="display-5 fw-bold">
           Fast and Simple Web Application Framework
         </h1>
         <p className="lead text-white-50">
           Handle thousands of requests per second with a minimalistic API
         </p>
-        <div className="d-flex p-2 bd-highlight justify-content-center">
+        <div className="d-none d-sm-flex p-2 bd-highlight justify-content-center">
           <div className="me-2">
             <a
               href="/start"
@@ -24,6 +24,24 @@ const Index = (props: { version: string }) => {
           </div>
           <div className="pt-2 pb-2 ps-3 pe-3 bg-black border border-light rounded fw-lighter align-middle">
             deno run -A -r https://fastro.dev
+          </div>
+        </div>
+
+        <div className="container d-block d-sm-none">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="mb-3">
+                <a
+                  href="/start"
+                  className="btn btn-lg btn-light fw-light bg-white w-100"
+                >
+                  Get started
+                </a>
+              </div>
+              <div className="pt-2 pb-2 ps-3 pe-3 bg-black border border-light rounded fw-lighter w-100">
+                deno run -A -r https://fastro.dev
+              </div>
+            </div>
           </div>
         </div>
       </main>
