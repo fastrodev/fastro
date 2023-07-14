5e57fc4e79731fc96e7e620ed678dc2698746a18
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-14 10:51:05 +0700

    debug2 deno deploy

--- http/deps.ts
+++ http/deps.ts
@@ -1,10 +1,8 @@
-// deno-lint-ignore no-deprecated-deno-api
-const ReactDOMServer = Deno.run === undefined
+const ReactDOMServer = Deno.env.get("DENO_DEPLOYMENT_ID")
   ? await import("https://esm.sh/react-dom@18.2.0/server")
   : await import("https://esm.sh/react-dom@18.2.0/server?dev");
 
-// deno-lint-ignore no-deprecated-deno-api
-const React = Deno.run === undefined
+const React = Deno.env.get("DENO_DEPLOYMENT_ID")
   ? await import("https://esm.sh/react@18.2.0")
   : await import("https://esm.sh/react@18.2.0?dev");
 
