2e4f2986272a4df193888a7e140745aa7ec8ebd4
Github Action <action@github.com>
2023-07-14 08:03:23 +0000

    chore: autopublish Fri Jul 14 08:03:23 UTC 2023

---
+++ hydrate/app.hydrate.tsx
@@ -0,0 +1,9 @@
+import { hydrateRoot } from "https://esm.sh/react-dom@18.2.0/client?dev";
+import { createElement } from "https://esm.sh/react@18.2.0?dev";
+import app from "../pages/app.tsx";
+// deno-lint-ignore no-explicit-any
+declare global { interface Window { __INITIAL_DATA__: any; } } 
+const props = window.__INITIAL_DATA__ || {};
+delete window.__INITIAL_DATA__ ;
+const el = createElement(app, props);
+hydrateRoot(document.getElementById("root") as Element, el);
