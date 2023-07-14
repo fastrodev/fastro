2228a0bddf7d1285e95f3679729cb54acf49a7e5
Github Action <action@github.com>
2023-07-14 08:22:27 +0000

    chore: autopublish Fri Jul 14 08:22:27 UTC 2023

--- hydrate/app.hydrate.tsx
+++ hydrate/app.hydrate.tsx
@@ -1,5 +1,5 @@
-import { hydrateRoot } from "https://esm.sh/react-dom@18.2.0/client?dev";
-import { createElement } from "https://esm.sh/react@18.2.0?dev";
+import { hydrateRoot } from "https://esm.sh/react-dom@18.2.0/client";
+import { createElement } from "https://esm.sh/react@18.2.0";
 import app from "../pages/app.tsx";
 // deno-lint-ignore no-explicit-any
 declare global { interface Window { __INITIAL_DATA__: any; } } 
