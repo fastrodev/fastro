69db7a0c83772b1852911b41cfbc44daba2faeda
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-14 15:18:57 +0700

    debug createHydrate2

--- http/render.tsx
+++ http/render.tsx
@@ -162,8 +162,12 @@ es.onmessage = function(e) {
 
   #createHydrate(comp: string, props?: any) {
     const component = comp.toLowerCase();
-    const dev = Deno.env.get("DENO_DEPLOYMENT_ID") ? "" : "?dev";
+    const dev = Deno.env.get("DENO_DEPLOYMENT_ID") === "" ? "" : "?dev";
     console.log("DEV=======>", dev);
+    console.log(
+      `Deno.env.get("DENO_DEPLOYMENT_ID")====>`,
+      Deno.env.get("DENO_DEPLOYMENT_ID"),
+    );
     return `import { hydrateRoot } from "https://esm.sh/react-dom@18.2.0/client${dev}";
 import { createElement } from "https://esm.sh/react@18.2.0${dev}";
 import ${component} from "../pages/${component}.tsx";
