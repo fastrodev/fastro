57ea5c1866a9047f7713efa0a53ad8fa063430bc
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-17 00:10:00 +0700

    update server_test

--- http/server_test.ts
+++ http/server_test.ts
@@ -8,30 +8,30 @@ Deno.test(
   async function getResponses() {
     const f = new fastro();
     f.get("/", () => new Response("get"));
-    // f.post("/", () => new Response("post"));
-    // f.put("/", () => new Response("put"));
-    // f.delete("/", () => new Response("delete"));
-    // f.options("/", () => new Response("options"));
-    // f.head("/", () => new Response(""));
+    f.post("/", () => new Response("post"));
+    f.put("/", () => new Response("put"));
+    f.delete("/", () => new Response("delete"));
+    f.options("/", () => new Response("options"));
+    f.head("/", () => new Response(""));
     f.serve();
 
     const get = await fetch(host, { method: "GET" });
     assertEquals(await get.text(), "get");
 
-    // const post = await fetch(host, { method: "POST" });
-    // assertEquals(await post.text(), "post");
+    const post = await fetch(host, { method: "POST" });
+    assertEquals(await post.text(), "post");
 
-    // const put = await fetch(host, { method: "PUT" });
-    // assertEquals(await put.text(), "put");
+    const put = await fetch(host, { method: "PUT" });
+    assertEquals(await put.text(), "put");
 
-    // const del = await fetch(host, { method: "DELETE" });
-    // assertEquals(await del.text(), "delete");
+    const del = await fetch(host, { method: "DELETE" });
+    assertEquals(await del.text(), "delete");
 
-    // const ops = await fetch(host, { method: "OPTIONS" });
-    // assertEquals(await ops.text(), "options");
+    const ops = await fetch(host, { method: "OPTIONS" });
+    assertEquals(await ops.text(), "options");
 
-    // const head = await fetch(host, { method: "HEAD" });
-    // assertEquals(await head.text(), "");
+    const head = await fetch(host, { method: "HEAD" });
+    assertEquals(await head.text(), "");
 
     f.close();
     await f.finished();
