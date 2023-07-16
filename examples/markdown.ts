15fa597740b62855987f4a0a5208b613c66e9deb
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-16 23:29:16 +0700

    remove markdown to middleware

--- examples/markdown.ts
+++ examples/markdown.ts
@@ -1,7 +1,10 @@
+import markdown from "../middlewares/markdown.tsx";
 import fastro from "../mod.ts";
 
 const f = new fastro();
+const m = new markdown();
 
+f.use(m.middleware);
 f.static("/static", { folder: "static", maxAge: 90 });
 
 await f.serve();
