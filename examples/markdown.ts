7d436d547fd6a3b28ad13cbac10972806885458c
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-17 19:28:48 +0700

    add markdown hook

--- examples/markdown.ts
+++ examples/markdown.ts
@@ -1,10 +1,10 @@
-import markdown from "../middlewares/markdown.tsx";
+import markdown from "../hooks/markdown.tsx";
 import fastro from "../mod.ts";
 
 const f = new fastro();
 const m = new markdown();
+f.hook(m.hook);
 
-f.use(m.middleware);
 f.static("/static", { folder: "static", maxAge: 90 });
 
 await f.serve();
