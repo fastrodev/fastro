3760580dd094445bcd900daea0a7235f8728d108
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-18 09:41:35 +0700

    update middleware_deno_kv 3

--- examples/middleware_deno_kv.ts
+++ examples/middleware_deno_kv.ts
@@ -5,28 +5,32 @@ const f = new fastro();
 
 // run deno with --unstable
 const kv = await Deno.openKv();
+// add initial data to deno kv
 kv.set(["users", "john"], { name: "john", id: uuid });
 f.record["kv"] = kv;
 
 // add parameterized endpoint with route level middleware
-f.get("/:user", (
-  _req: HttpRequest,
+f.get("/:user", async (
+  req: HttpRequest,
   _ctx: Context,
   next: Next,
 ) => {
+  const kv = req.record["kv"] as Deno.Kv;
+
+  // get existing data
+  const key: string = req.query?.name ?? "";
+  const u = await kv.get(["users", key]);
+  const data = u.value as object;
+
+  // add path to object
+  const path = { path: req.url };
+  kv.set(["users", key], { ...data, ...path });
+
   return next();
 }, async (req: HttpRequest, _ctx: Context) => {
   // get the initialized kv data
   const kv: Deno.Kv = req.record["kv"];
-  const key: string = req.query?.name ? req.query?.name : "";
-  const u = await kv.get(["users", key]);
-  return Response.json(u.value);
-});
-
-f.post("/:user", async (req: HttpRequest, _ctx: Context) => {
-  // get the initialized kv data
-  const kv: Deno.Kv = req.record["kv"];
-  const key: string = req.query?.name ? req.query?.name : "";
+  const key: string = req.query?.name ?? "";
   const u = await kv.get(["users", key]);
   return Response.json(u.value);
 });
