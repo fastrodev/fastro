a6ba50b23e8892da4dd92ed2458337d31a3f3ab0
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-25 15:36:13 +0700

    refactor(header): add nav-scroller

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,27 +3,10 @@ import React from "https://esm.sh/react@18.2.0?dev";
 export default function (props: { version: string }) {
   return (
     <footer className="mt-auto text-center fw-lighter text-white-50">
-      <nav className="nav nav-mastfoot justify-content-center">
-        <a
-          className={`nav-link py-1 px-0 text-decoration-none`}
-          href="/benchmarks"
-        >
-          Benchmarks
-        </a>
-        <a
-          className={`nav-link py-1 px-0 text-decoration-none text-white-50`}
-          href="/examples"
-        >
-          Examples
-        </a>
-        <a
-          className={`nav-link py-1 px-0 text-decoration-none text-white-50`}
-          href="#"
-        >
-          Blog
-        </a>
-      </nav>
-
+      {
+        /* <nav className="nav nav-mastfoot justify-content-center">
+      </nav> */
+      }
       <span className="fw-lighter">
         Made with{"  "}
         <a
