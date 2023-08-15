d17e67a848176b8d684b02cdda8895480d6dd7d4
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-08-16 05:41:48 +0700

    update server_test.ts

--- http/server_test.ts
+++ http/server_test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.198.0/assert/assert_equals.ts";
 import { assertExists } from "https://deno.land/std@0.198.0/assert/assert_exists.ts";
-import fastro from "../mod.ts";
+import fastro, { Fastro, Info } from "../mod.ts";
 import { assert } from "https://deno.land/std@0.198.0/assert/assert.ts";
 
 const host = "http://localhost:8000";
@@ -9,12 +9,17 @@ Deno.test(
   { permissions: { net: true, env: true, read: true, write: true } },
   async function getResponses() {
     const f = new fastro();
+    f.hook((_f: Fastro, r: Request, _i: Info) => {
+      if (r.url === "hook") return new Response("hook");
+      return new Response();
+    });
     f.get("/", () => new Response("get"));
     f.post("/", () => new Response("post"));
     f.put("/", () => new Response("put"));
     f.delete("/", () => new Response("delete"));
     f.options("/", () => new Response("options"));
     f.head("/", () => new Response(""));
+    f.get("/hook", () => new Response("hook"));
     f.serve();
 
     const get = await fetch(host, { method: "GET" });
@@ -35,19 +40,9 @@ Deno.test(
     const head = await fetch(host, { method: "HEAD" });
     assertEquals(await head.text(), "");
 
-    f.close();
-    await f.finished();
-  },
-);
+    const hook = await fetch(host + "/hook", { method: "GET" });
+    assertEquals(await hook.text(), "hook");
 
-Deno.test(
-  { permissions: { net: true, env: true, read: true, write: true } },
-  async function getStaticFileWithStaticPath() {
-    const f = new fastro();
-    f.hook(() => new Response("hook"));
-    f.serve();
-    const get = await fetch(host, { method: "GET" });
-    assertEquals(await get.text(), "hook");
     f.close();
     await f.finished();
   },
