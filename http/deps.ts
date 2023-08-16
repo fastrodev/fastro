8d35843a0346fae9fbcb0427ef38b5d2b9298203
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-08-16 14:57:08 +0700

    preact-render-to-string: update specifier

--- http/deps.ts
+++ http/deps.ts
@@ -1,7 +1,7 @@
 export { h } from "https://esm.sh/preact@10.16.0";
 export {
   default as renderToString,
-} from "https://esm.sh/preact-render-to-string?deps=preact@10.16.0";
+} from "https://esm.sh/preact-render-to-string@6.2.1?deps=preact@10.16.0";
 
 export { toHashString } from "https://deno.land/std@0.198.0/crypto/to_hash_string.ts";
 export {
