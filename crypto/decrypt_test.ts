e6143ce5b672db63e08b830b70e5de6246ba30b4
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-08-18 10:01:33 +0700

    update decrypt test

--- crypto/decrypt_test.ts
+++ crypto/decrypt_test.ts
@@ -4,7 +4,6 @@ import { encryptData } from "./encrypt.ts";
 import { exportCryptoKey, importCryptoKey, keyPromise } from "./key.ts";
 
 Deno.test(
-  { permissions: { net: true, env: true, read: true, write: true } },
   async function Decrypt() {
     const keyType = "AES-GCM";
     const keyUsages: KeyUsage[] = ["encrypt", "decrypt"];
@@ -17,11 +16,9 @@ Deno.test(
       keyUsages,
     );
     const plaintext = "Hello, encryption!";
-
     const encryptedData = await encryptData(importedKey, plaintext);
-    const decryptedText = await decryptData(importedKey, encryptedData);
-
-    console.log("Decrypted Text:", decryptedText);
-    assert(decryptedText, "get");
+    decryptData(importedKey, encryptedData).then((decryptedText) => {
+      assert(decryptedText, "get");
+    });
   },
 );
