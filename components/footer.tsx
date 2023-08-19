adfa265767e90152c9f3b3c42d69ccb182706683
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-08-19 16:04:40 +0700

    update layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,9 +5,8 @@ export default function (props: { version: string; path: string }) {
   return (
     <footer className="mt-auto">
       <nav className="nav nav-mastfoot justify-content-center">
-        {/* <LinkFooter link="/benchmarks">Benchmarks</LinkFooter> */}
         <LinkFooter link="https://github.com/fastrodev/fastro">
-          Github
+          Repository
         </LinkFooter>
         <LinkFooter link="https://deno.land/x/fastro">
           Fastro{"  "}{props.version}
