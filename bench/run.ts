893c93273cd9ed49106ddb3e997c5192af5e4e58
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-08-22 01:19:11 +0700

    bench: add toLocaleString

--- bench/run.ts
+++ bench/run.ts
@@ -132,7 +132,9 @@ description: This is the final output of an internal benchmark run on github act
 image: https://fastro.dev/static/image.png
 ---
 
-This is the final output of an internal benchmark run on [github action](https://github.com/fastrodev/fastro/actions). It consists of several simple applications for [specific purpose](https://github.com/fastrodev/fastro/blob/main/deno.json). Each is then accessed by the [OHA](https://github.com/hatoo/oha) within 5 seconds. The results are then sorted by the fastest.
+This is the final output of an internal benchmark run in [github action](https://github.com/fastrodev/fastro/actions) on \`${
+  new Date().toLocaleString()
+}\`. It consists of several simple applications for [specific purpose](https://github.com/fastrodev/fastro/blob/main/deno.json). Each is then accessed by the [OHA](https://github.com/hatoo/oha) within 5 seconds. The results are then sorted by the fastest.
 
 You can find the benchmark script in this code: [run.ts](https://github.com/fastrodev/fastro/blob/main/bench/run.ts)
 
