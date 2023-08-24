c78a19e7b12fa158edeeaae4f8c5419cf10fa702
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-08-24 14:49:08 +0700

    update STATUS_TEXT & toHashString

--- http/deps.ts
+++ http/deps.ts
@@ -5,11 +5,11 @@ export {
   default as renderToString,
 } from "https://esm.sh/preact-render-to-string@6.2.1?deps=preact@10.16.0";
 
-export { toHashString } from "https://deno.land/std@0.198.0/crypto/to_hash_string.ts";
+export { toHashString } from "https://deno.land/std@0.199.0/crypto/to_hash_string.ts";
 export {
   Status,
   STATUS_TEXT,
-} from "https://deno.land/std@0.198.0/http/http_status.ts";
+} from "https://deno.land/std@0.199.0/http/http_status.ts";
 export * from "https://deno.land/std@0.199.0/media_types/mod.ts";
 export * from "https://deno.land/std@0.199.0/path/mod.ts";
 
