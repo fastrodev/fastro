002f2c06a203ee122c46efd08140264912db798d
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-08-28 00:37:09 +0700

    update build deps

--- build/deps.ts
+++ build/deps.ts
@@ -1,7 +1,4 @@
 // @deno-types="https://deno.land/x/esbuild@v0.18.11/mod.d.ts"
 import * as esbuild from "https://deno.land/x/esbuild@v0.18.11/mod.js";
-
-const esbuildWasmURL = new URL("./esbuild_v0.18.11.wasm", import.meta.url).href;
-
 export { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.ts";
-export { esbuild, esbuildWasmURL };
+export { esbuild };
