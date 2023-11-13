d288a21996aa4591f57e908f5b1d62ab70e6e82e
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-11-13 22:30:17 +0700

    chore: add ssr with custom folder

---
+++ examples/ssr_with_custom_folder.ts
@@ -0,0 +1,23 @@
+import fastro, { Context, HttpRequest } from "../mod.ts";
+import uuid from "../uuid/uuid.page.tsx";
+import { uuidService } from "../uuid/uuid.service.ts";
+
+export function apiHandler(_req: Request) {
+  return uuidService();
+}
+
+const getUser = (data: string) => Promise.resolve(data);
+const handler = async (_req: HttpRequest, ctx: Context) => {
+  const data = await getUser("Guest");
+  return ctx.render({
+    props: { data, title: "React Component" },
+    status: 200,
+  });
+};
+
+const f = new fastro();
+f.static("/static", { folder: "static" });
+f.page("/", { component: uuid, folder: "uuid" }, handler);
+f.get("/api", apiHandler);
+
+await f.serve();
