059758e9113e3cf6f0fcd38835d4769be22bfd8f
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-11-17 09:38:56 +0700

    chore: update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,22 +14,8 @@ export default function (props: { version: string; path: string }) {
         <LinkFooter link="/middlewares">
           Middlewares
         </LinkFooter>
-        <LinkFooter link="/signin">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-person-circle"
-            viewBox="0 0 16 16"
-          >
-            <title>Repository</title>
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path
-              fill-rule="evenodd"
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-            />
-          </svg>
+        <LinkFooter link="https://deno.land/x/fastro">
+          {props.version}
         </LinkFooter>
       </nav>
     </footer>
