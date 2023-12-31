08e8a1d8421ad6a9614ff2d96ae80ac0a5f72dfe
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-12-31 19:54:25 +0700

    update hello.page

--- app/hello.page.tsx
+++ app/hello.page.tsx
@@ -1,5 +1,7 @@
 import { PageProps } from "../src/server/types.ts";
 
-export default function Hello(props: PageProps<string>) {
-  return <p>FN {props.data}</p>;
+export default function Hello(
+  { data }: PageProps<{ data: string; user: string; title: string }>,
+) {
+  return <p>FN {data.data}</p>;
 }
