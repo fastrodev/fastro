b49bc76c1b28125d2c24d10d0bb7c2b631fcd9a0
yanu <yanu.widodo@accenture.com>
2024-01-27 08:14:16 +0700

    update index.page.tsx

--- modules/web/main.tsx
+++ modules/web/main.tsx
@@ -54,7 +54,7 @@ s.page("/", {
       title:
         "Don't settle for the status quo. Embrace the future of web development",
       description:
-        "With the power of Deno and TypeScript, Fastro packages Preact JS and Tailwind CSS, providing a unified experience. It also gives you the modular structure necessary for building successful applications.",
+        "With the power of Deno and TypeScript, Fastro packages Preact JS and Tailwind CSS, providing a unified experience.",
       youtube: "https://www.youtube.com/embed/cZc4Jn5nK3k",
       image:
         "https://avatars.githubusercontent.com/u/84224795?s=400&u=a53076f3dac46609e2837bef9980ae22ecd86e62&v=4",
