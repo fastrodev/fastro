116e44611a5f6f76bd01df5c8a6347dc24a6b078
yanu <yanu.widodo@accenture.com>
2024-01-31 11:43:40 +0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,29 +3,28 @@ export function Footer() {
     <div
       class={`container max-w-4xl mx-auto text-center text-xs font-extralight py-3 px-6 dark:text-gray-400 border-t  dark:border-gray-700`}
     >
-      Fastro Framework is powered by{" "}
-      <a
-        href={`https://deno.com/deploy`}
-        class={`text-blue-600 dark:text-blue-500 hover:underline`}
-      >
-        Deno Deploy
-      </a>
-      {" & "}
+      Fastro Framework is{" "}
       <a
         href={`https://github.com/fastrodev/fastro`}
         class={`text-blue-600 dark:text-blue-500 hover:underline`}
       >
-        Github Repository.
+        free and open source.
       </a>{" "}
-      Key metrics of the use case, Requests per seconds (RPS), have been
-      assessed in{"  "}
+      Requests per second (RPS) for each use case is monitored daily through
+      {"  "}
       <a
         href={`/docs/benchmarks`}
         class={`text-blue-600 dark:text-blue-500 hover:underline`}
       >
         internal benchmarks.
+      </a>{"  "}
+      Powered by{" "}
+      <a
+        href={`https://deno.com/deploy`}
+        class={`text-blue-600 dark:text-blue-500 hover:underline`}
+      >
+        Deno Deploy.
       </a>
-      {"  "}
     </div>
   );
 }
