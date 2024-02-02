3b303f2c6d391fde02e7e545b164fc1c6864b1d2
yanu <yanu.widodo@accenture.com>
2024-02-03 01:59:14 +0700

    update init.ts

--- static/init.ts
+++ static/init.ts
@@ -3,16 +3,16 @@ async function clone(name?: string) {
     const folder = name ?? "project";
     const args = ["clone", "https://github.com/fastrodev/template.git", folder];
     const command = new Deno.Command("git", { args });
-    const remove = new Deno.Command("git", {
-      args: ["remote", "rm", "origin"],
-    });
     const { stderr } = await command.output();
-    await remove.output();
     const err = new TextDecoder().decode(stderr);
     console.log(err);
     console.log(`%cEnter:`, "color:blue");
     console.log(`cd ${folder}`);
     console.log(`deno task start`);
+    const remove = new Deno.Command("git", {
+      args: ["remote", "rm", "origin"],
+    });
+    await remove.output();
   } catch (error) {
     console.error(error);
   }
