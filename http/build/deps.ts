905c4f943f269d8145be87c2882e79c895f3ef38
yanu <yanuwid@gmail.com>
2024-04-11 17:25:38 +0700

    chore: update esbuild

--- http/build/deps.ts
+++ http/build/deps.ts
@@ -1,4 +1,4 @@
-// @deno-types="https://deno.land/x/esbuild@v0.20.0/mod.d.ts"
-import * as esbuild from "https://deno.land/x/esbuild@v0.20.0/mod.js";
-export { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.5/mod.ts";
+// @deno-types="https://deno.land/x/esbuild@v0.20.2/mod.d.ts"
+import * as esbuild from "https://deno.land/x/esbuild@v0.20.2/mod.js";
+export { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.9.0/mod.ts";
 export { esbuild };
