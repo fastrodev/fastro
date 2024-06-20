9b12664d0d4667b483ebc1f6c8ac220f330e19b6
yanu <yanuwid@gmail.com>
2024-06-20 18:07:14 +0700

    update run.ts

--- bench/run.ts
+++ bench/run.ts
@@ -128,9 +128,11 @@ const t = table.map((v) => {
 });
 
 let markdown = `---
-title: "Benchmarks"
+title: Benchmarks
 description: This is the final output of an internal benchmark run in github action
 image: https://fastro.dev/fastro.png
+previous: structure
+next: ""
 ---
 
 This is the final output of an internal benchmark run in [github action](https://github.com/fastrodev/fastro/actions) on \`${
