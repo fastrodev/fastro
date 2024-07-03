9a1bbd00e2d6bef8d945d6270a508cbfc269e6ae
ynwd <10122431+ynwd@users.noreply.github.com>
2024-07-03 17:06:51 +0700

    chore: update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,23 +1,3 @@
-function HeartSvg() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="icon icon-tabler icons-tabler-outline icon-tabler-heart"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
-    </svg>
-  );
-}
-
 export function Footer() {
   return (
     <div
@@ -30,10 +10,7 @@ export function Footer() {
       >
         free and open source
       </a>
-      {" full stack web framework for Deno, TypeScript, Preact, and Tailwind. "}
-      <div class={`inline-flex gap-1 items-center`}>
-        <p>Made in Tulungagung with</p> <HeartSvg />
-      </div>
+      {" full stack web framework for Deno, TypeScript, Preact JS, and Tailwind CSS. Made with ♡ in Tulungagung"}
     </div>
   );
 }
