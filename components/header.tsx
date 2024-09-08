473ea035029a880dbab1b5ad498d13a2195189b3
ynwd <10122431+ynwd@users.noreply.github.com>
2024-09-08 09:55:34 +0700

    chore: update header

--- components/header.tsx
+++ components/header.tsx
@@ -16,18 +16,20 @@ export default function Header(
         <div
             class={`container flex justify-between max-w-4xl mx-auto text-center text-sm py-6 px-3 xl:px-0 md:px-0 sm:px-0 dark:text-gray-400`}
         >
-            <a href="/" class={`flex space-x-2 items-center dark:text-white`}>
-                <div
-                    class={`border-[2px] border-gray-400 rounded-full p-[4px]`}
-                >
-                    {props.isLogin
-                        ? <RocketSvg />
-                        : props.previous_url
-                        ? <AngleLeftSvg />
-                        : <BoltSvg />}
-                </div>
+            <div class={`flex space-x-2 items-center text-white`}>
+                <a href="/" class={`text-white`}>
+                    <div
+                        class={`border-[2px] border-gray-400 rounded-full p-[4px]`}
+                    >
+                        {props.isLogin
+                            ? <RocketSvg />
+                            : props.previous_url
+                            ? <AngleLeftSvg />
+                            : <BoltSvg />}
+                    </div>
+                </a>
                 <span>{`${props.title || "Fastro"}`}</span>
-            </a>
+            </div>
             <div class={`flex items-center space-x-3`}>
                 <a class={`dark:text-white`} href={"/blog"}>Blog</a>
                 <a class={`dark:text-white`} href={"/docs"}>Docs</a>
