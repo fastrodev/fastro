0d8cfa472e5c6570fe46f938c754795377631244
ynwd <10122431+ynwd@users.noreply.github.com>
2024-09-26 16:37:47 +0700

    chore: update getSHA

--- core/map/mod.ts
+++ core/map/mod.ts
@@ -275,7 +275,6 @@ async function getSHA(options: StoreOptions): Promise<string | undefined> {
             repo: options.repo,
             path: options.path,
             ref: options.branch,
-            mediaType: { format: "raw" },
         }) as any;
 
         return response.data.sha;
