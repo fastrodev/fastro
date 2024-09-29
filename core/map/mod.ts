497f943acd04e8697206dc8877c337647b9a48ef
ynwd <10122431+ynwd@users.noreply.github.com>
2024-09-30 06:41:14 +0700

    feat(utils): add octokit wrapper

--- core/map/mod.ts
+++ core/map/mod.ts
@@ -1,13 +1,10 @@
-// deno-lint-ignore-file no-explicit-any
-import { Octokit } from "npm:@octokit/rest";
-
-type StoreOptions = {
-    token?: string;
-    owner: string;
-    repo: string;
-    path: string;
-    branch?: string;
-} | null;
+import {
+    deleteGithubFile,
+    getMap,
+    mapToRecord,
+    type StoreOptions,
+    uploadFileToGitHub,
+} from "../../utils/octokit.ts";
 
 export class Store<K extends string | number | symbol, V> {
     private map: Map<K, { value: V; expiry?: number }>;
@@ -243,120 +240,5 @@ export class Store<K extends string | number | symbol, V> {
                 this.map.delete(key);
             }
         }
-    }
-}
-
-function recordToMap<K extends string | number | symbol, V>(
-    record: Record<K, { value: V; expiry: number }>,
-): Map<K, { value: V; expiry?: number }> {
-    const map = new Map<K, { value: V; expiry: number }>();
-
-    Object.entries(record).forEach(([key, value]) => {
-        const entry = value as { value: V; expiry: number };
-        map.set(key as K, entry);
-    });
-
-    return map;
-}
-
-function mapToRecord<K extends string | number | symbol, V>(
-    map: Map<K, V>,
-): Record<K, V> {
-    return Object.fromEntries(map) as Record<K, V>;
-}
-
-async function getSHA(options: StoreOptions): Promise<string | undefined> {
-    if (!options || !options.token) throw new Error("GITHUB_TOKEN is needed");
-    const octokit = new Octokit({ auth: options.token });
-    try {
-        const response = await octokit.repos.getContent({
-            owner: options.owner,
-            repo: options.repo,
-            path: options.path,
-            ref: options.branch,
-        }) as any;
-
-        return response.data.sha;
-    } catch (error) {
-        throw error;
-    }
-}
-
-async function uploadFileToGitHub(
-    options: {
-        token?: string;
-        owner: string;
-        repo: string;
-        path: string;
-        content: string;
-        branch?: string;
-    },
-) {
-    const octokit = new Octokit({ auth: options.token });
-    try {
-        const res = await getFileFromGithub(options);
-        const sha = res ? await getSHA(options) : undefined;
-        const message = res
-            ? `Update ${options.path}`
-            : `Create ${options.path}`;
-        return await octokit.repos.createOrUpdateFileContents({
-            owner: options.owner,
-            repo: options.repo,
-            path: options.path,
-            message,
-            content: btoa(options.content),
-            sha,
-            branch: options.branch,
-        });
-    } catch (error) {
-        throw error;
-    }
-}
-
-async function getMap<K extends string | number | symbol, V>(
-    options: StoreOptions,
-) {
-    if (!options || !options.token) return;
-    const res = await getFileFromGithub(options) as any;
-    if (res) return recordToMap<K, V>(JSON.parse(res));
-}
-
-/**
- * Gets the contents of a file in a repository (1-100 MB)
- */
-async function getFileFromGithub(options: StoreOptions) {
-    if (!options || !options.token) throw new Error("GITHUB_TOKEN is needed");
-    const octokit = new Octokit({ auth: options.token });
-    try {
-        const res = await octokit.repos.getContent({
-            owner: options.owner,
-            repo: options.repo,
-            path: options.path,
-            ref: options.branch,
-            mediaType: { format: "raw" },
-        });
-        return res.data;
-    } catch {
-        return undefined;
-    }
-}
-
-async function deleteGithubFile(options: StoreOptions) {
-    if (!options || !options.token) throw new Error("GITHUB_TOKEN is needed");
-    const octokit = new Octokit({ auth: options.token });
-    try {
-        const sha = await getSHA(options);
-        if (!sha) throw new Error("SHA is needed");
-        const res = await octokit.repos.deleteFile({
-            owner: options.owner,
-            repo: options.repo,
-            path: options.path,
-            sha,
-            branch: options.branch,
-            message: `Delete ${options.path}`,
-        });
-        return res.data;
-    } catch (error) {
-        throw error;
     }
 }
