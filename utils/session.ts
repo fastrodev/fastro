fb6b3e7a2823ac295b0823619372e6619242cc16
ynwd <10122431+ynwd@users.noreply.github.com>
2024-10-06 07:24:34 +0700

    chore(utils): update session.ts

--- utils/session.ts
+++ utils/session.ts
@@ -6,7 +6,7 @@ export async function getSession(req: HttpRequest, ctx: Context) {
     const sessionId = await getSessionId(req);
     if (!sessionId) return undefined;
 
-    const r = ctx.server.store.get(sessionId) as any;
+    const r = await ctx.server.store.get(sessionId) as any;
     const avatar_url = r ? r.avatar_url : null;
     const html_url = r ? r.html_url : null;
     const isLogin = r ? true : false;
