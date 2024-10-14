cc02f6095ee6871660fc6c2a9ef9918d6c2a5acb
ynwd <10122431+ynwd@users.noreply.github.com>
2024-10-14 20:22:11 +0700

    chore: update preact

--- core/server/deps.ts
+++ core/server/deps.ts
@@ -6,14 +6,14 @@ export * from "jsr:@std/path@^1.0.1";
 export { encodeHex } from "jsr:@std/encoding@^1.0.5/hex";
 export { assert, assertEquals, assertExists } from "jsr:@std/assert@^1.0.6";
 
-export { h } from "https://esm.sh/preact@10.24.2";
+export { h } from "https://esm.sh/preact@10.24.3";
 export type {
   ComponentChild,
   ComponentChildren,
   JSX,
   VNode,
-} from "https://esm.sh/preact@10.24.2";
+} from "https://esm.sh/preact@10.24.3";
 export {
   renderToString,
   renderToStringAsync,
-} from "https://esm.sh/preact-render-to-string@6.5.9?deps=preact@10.24.2";
+} from "https://esm.sh/preact-render-to-string@6.5.9?deps=preact@10.24.3";
