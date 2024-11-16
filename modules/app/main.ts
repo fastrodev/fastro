e0ea7872646093aab54c667638aa5dc9976b71e0
ynwd <10122431+ynwd@users.noreply.github.com>
2024-11-16 20:51:17 +0700

    chore: update kv

--- modules/app/main.ts
+++ modules/app/main.ts
@@ -15,6 +15,8 @@ import roomModules from "@app/modules/room/mod.ts";
 
 const s = new Server();
 
+console.log(Deno.env.get("DENO_KV_ACCESS_TOKEN"));
+
 /** markdown with default folder and prefix */
 s.use(markdownDocs(blogLayout, "post", "blog"));
 
--- utils/kv.ts
+++ utils/kv.ts
@@ -5,9 +5,21 @@ if (args === "reset") {
     await reset();
 }
 
-const key = args ? [args] : [];
-const entries = kv.list({ prefix: key });
+if (args === "deploy") {
+    const kd = await Deno.openKv(
+        "https://api.deno.com/databases/8ce5f998-0c99-465e-971b-b8911d896fed/connect",
+    );
 
-for await (const entry of entries) {
-    console.log(`[${entry.key}]:`, entry.value);
+    getAll(kd);
+} else {
+    getAll(kv);
+}
+
+async function getAll(kv: Deno.Kv) {
+    const key = args ? [args] : [];
+    const entries = kv.list({ prefix: key });
+
+    for await (const entry of entries) {
+        console.log(`[${entry.key}]:`, entry.value);
+    }
 }
