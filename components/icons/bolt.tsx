5f4bfe60e184ffd5fb7783e6b682ff8a37063588
ynwd <10122431+ynwd@users.noreply.github.com>
2024-12-04 06:49:55 +0700

    chore: update bolt.svg

--- components/icons/bolt.tsx
+++ components/icons/bolt.tsx
@@ -7,7 +7,7 @@ export default function BoltSvg(props: { height?: string; width?: string }) {
       viewBox="327.782 -762.789 303.276 564.689"
       width={props.width || 18}
     >
-      <path d="M 456 -200 L 630 -540 L 510 -540 L 510 -760 L 330 -420 L 456 -420 L 456 -200 Z M 480 -400 Z">
+      <path d="M 426.255 -198.392 L 630 -540 L 510 -540 L 536.033 -761.164 L 330 -420 L 456 -420 L 426.255 -198.392 Z M 480 -400 Z">
       </path>
     </svg>
   );
