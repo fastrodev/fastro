ae7c4c7bc9880f2390ceb0539f8711a54ecb3d94
ynwd <10122431+ynwd@users.noreply.github.com>
2025-02-24 08:10:47 +0700

    refactor(landing-page): update icons

--- components/icons/CategoryIcons.tsx
+++ components/icons/CategoryIcons.tsx
@@ -249,16 +249,12 @@ export const CategoryIcons = {
       stroke-linecap="round"
       stroke-linejoin="round"
       className={className}
-      class="icon icon-tabler icons-tabler-outline icon-tabler-brand-codesandbox"
+      class="icon icon-tabler icons-tabler-outline icon-tabler-webhook"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M20 7.5v9l-4 2.25l-4 2.25l-4 -2.25l-4 -2.25v-9l4 -2.25l4 -2.25l4 2.25z" />
-      <path d="M12 12l4 -2.25l4 -2.25" />
-      <path d="M12 12l0 9" />
-      <path d="M12 12l-4 -2.25l-4 -2.25" />
-      <path d="M20 12l-4 2v4.75" />
-      <path d="M4 12l4 2l0 4.75" />
-      <path d="M8 5.25l4 2.25l4 -2.25" />
+      <path d="M4.876 13.61a4 4 0 1 0 6.124 3.39h6" />
+      <path d="M15.066 20.502a4 4 0 1 0 1.934 -7.502c-.706 0 -1.424 .179 -2 .5l-3 -5.5" />
+      <path d="M16 8a4 4 0 1 0 -8 0c0 1.506 .77 2.818 2 3.5l-3 5.5" />
     </svg>
   ),
 
