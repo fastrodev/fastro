6ad1e70ad4def73e4e134f98410496dce941c898
ynwd <10122431+ynwd@users.noreply.github.com>
2025-04-25 09:13:50 +0700

    chore: update esbuild

--- core/build/deps.ts
+++ core/build/deps.ts
@@ -1,3 +1,3 @@
-import * as esbuild from "npm:esbuild@0.24.0";
+import * as esbuild from "npm:esbuild@0.25.3";
 export { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.11.0";
 export { esbuild };
