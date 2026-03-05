551dbb607f33e48683478760ae5cb5203c4d6f37
ynwd <10122431+ynwd@users.noreply.github.com>
2026-03-05 22:18:09 +0700

    fix: use dynamic import to ensure __VERSION__ is set before katex evaluation

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 // @ts-ignore: define global constant for KaTeX
 globalThis.__VERSION__ = "0.16.11";
-import app from "./app.ts";
+
+const { default: app } = await import("./app.ts");
 
 app.serve({ port: parseInt(Deno.args[0]) || 8000 });
